c822b134bc06ef715b7094b6cd484f36134aa4f4
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-01-25 09:46:33 -0800

    merging archived items

---
+++ ArchaeaMod/Merged/Buffs/magno_summon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArchaeaMod.Merged.Buffs
+{
+    class magno_summon : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Magno Fly");
+            Description.SetDefault("Chemically enhanced minion");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[mod.ProjectileType("magno_minion")] == 0)
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+            else
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+        }
+    }
+}
