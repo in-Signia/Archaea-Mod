0457695656c55e3d1d4b271ef8a965de8f4f4129
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-01-26 20:21:31 -0800

    sky brick
    - Dark and light versions

---
+++ ArchaeaMod/Tiles/ArchaeaTiles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArchaeaMod.Tiles
+{
+    public class ArchaeaTiles : GlobalTile
+    {
+        public override bool CanExplode(int i, int j, int type)
+        {
+            for (int k = -1; k <= 1; k++)
+                for (int l = -1; l <= 1; l++)
+                {
+                    Tile tile = Main.tile[i + k, j + l];
+                    if (tile.type == ArchaeaWorld.crystal ||
+                        tile.type == ArchaeaWorld.crystal2x1 ||
+                        tile.type == ArchaeaWorld.crystal2x2)
+                        return false;
+                }
+            return base.CanExplode(i, j, type);
+        }
+        public override bool CanKillTile(int i, int j, int type, ref bool blockDamaged)
+        {
+            for (int k = -1; k <= 1; k++)
+                for (int l = -1; l <= 1; l++)
+                {
+                    Tile tile = Main.tile[i + k, j + l];
+                    if (tile.type == ArchaeaWorld.crystal ||
+                        tile.type == ArchaeaWorld.crystal2x1 ||
+                        tile.type == ArchaeaWorld.crystal2x2)
+                        return false;
+                }
+            return base.CanKillTile(i, j, type, ref blockDamaged);
+        }
+    }
+}
---
+++ ArchaeaMod/Tiles/sky_lightbrick.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArchaeaMod.Tiles
+{
+    public class sky_lightbrick : ModTile
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            return false;
+        }
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = false;
+            drop = mod.ItemType("sky_lightbrick");
+            AddMapEntry(Color.AntiqueWhite);
+            soundStyle = 0;
+            soundType = 21;
+        }
+        public override void WalkDust(ref int dustType, ref bool makeDust, ref Color color)
+        {
+            dustType = mod.DustType<Merged.Dusts.c_silver_dust>();
+            makeDust = true;
+            color = Color.White;
+        }
+    }
+}
